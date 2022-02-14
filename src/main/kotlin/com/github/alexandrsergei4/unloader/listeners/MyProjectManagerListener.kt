f946a22bee2526f3240d8d47dfceb39f6843a0d3
GitHub Action <action@github.com>
2022-02-14 11:10:29 +0000

    Template cleanup

--- src/main/kotlin/com/github/alexandrsergei4/unloader/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/alexandrsergei4/unloader/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.alexandrsergei4.unloader.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.alexandrsergei4.unloader.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
